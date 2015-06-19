140ef1047040eb7a862c1bc34ad64933199bacb5
Julien Pagès <j.parkouss@gmail.com>
2015-06-19 19:09:42 +0200

    rename some stuff
    - rename driver fixture to browser
    - driver option is now called webdriver and is a fixture

--- pynium/plugin.py
+++ pynium/plugin.py
@@ -1,15 +1,23 @@
-from pynium import webdriver
+from pynium.webdriver import extended as extended_webdriver
 import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--driver", action="store", default='Firefox',
+    parser.addoption("--webdriver", action="store", default='Firefox',
                      help="Web driver to use, default to %(default)s")
 
 
+@pytest.fixture(scope='session')
+def webdriver(request):
+    """
+    webdriver name fixture.
+    """
+    return request.config.option.webdriver
+
+
 @pytest.yield_fixture(scope='session')
-def driver(request):
-    browser_class = webdriver.extended(request.config.getoption("--driver"))
+def browser(webdriver):
+    browser_class = extended_webdriver(webdriver)
     browser = browser_class()
     yield browser
     browser.quit()
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,19 +1,19 @@
 from selenium.webdriver.common.keys import Keys
 
 
-def test_one(driver):
-    driver.get("http://www.python.org")
-    assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
+def test_one(browser):
+    browser.get("http://www.python.org")
+    assert "Python" in browser.title
+    elem = browser.find_element_by_name("q")
     elem.send_keys("pycon")
     elem.send_keys(Keys.RETURN)
-    assert "No results found." not in driver.page_source
+    assert "No results found." not in browser.page_source
 
 
-def test_two(driver):
+def test_two(browser):
     # logic extracted from code generation using
     # firefox selenium plugin:
     # http://www.seleniumhq.org/download/
-    driver.get("http://try.jquery.com/")
-    driver.find_element_by_css_selector("b").click()
-    assert driver.find_element_by_link_text("What is jQuery?") is not None
+    browser.get("http://try.jquery.com/")
+    browser.find_element_by_css_selector("b").click()
+    assert browser.find_element_by_link_text("What is jQuery?") is not None
