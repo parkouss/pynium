34a79dacf7b9e6912ca3851752f699d2816d5260
Julien Pagès <j.parkouss@gmail.com>
2015-06-20 07:29:48 +0200

    add tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -13,10 +13,69 @@ def testit(request, testdir, http_server, tests_dir):
 
 
 def test_simple_page(testit):
-    recorder = testit.inline_runsource(py.code.Source(r"""
+    testit.makepyfile(py.code.Source(r"""
         def test_simple_page(browser):
             browser.get(%r)
             elem = browser.find_element_by_tag_name("h1")
             assert elem.text == 'My First Heading'
 """ % (testit.base_url + '/simple.html')))
-    recorder.assertoutcome(passed=1)
+    result = testit.runpytest()
+    assert result.parseoutcomes().get('passed') == 1
+
+
+def test_error_should_take_screenshot(testit):
+    testit.makepyfile(py.code.Source(r"""
+        def test_simple_page(browser):
+            browser.get(%r)
+            elem = browser.find_element_by_id("thisdoesnotexist")
+""" % (testit.base_url + '/simple.html')))
+    result = testit.runpytest()
+    assert result.parseoutcomes().get('failed') == 1
+    assert testit.tmpdir.join("test_error_should_take_screenshot",
+                              "test_simple_page-browser.png").exists()
+
+
+def test_error_should_not_take_screenshot_if_specified(testit):
+    testit.makepyfile(py.code.Source(r"""
+        def test_simple_page(browser):
+            browser.get(%r)
+            elem = browser.find_element_by_id("thisdoesnotexist")
+""" % (testit.base_url + '/simple.html')))
+    result = testit.runpytest('--make-screenshot-on-failure=false')
+    assert result.parseoutcomes().get('failed') == 1
+    assert not testit.tmpdir.join("test_error_should_take_screenshot",
+                                  "test_simple_page-browser.png").exists()
+
+
+def test_one_browser_by_test_session(testit):
+    testit.makepyfile(py.code.Source(r"""
+        import pytest
+        USED_BROWSER = None
+        @pytest.mark.run(order=1)
+        def test_first(browser):
+            global USED_BROWSER
+            browser.get(%r)
+            USED_BROWSER = browser
+        @pytest.mark.run(order=2)
+        def test_second(browser):
+            browser.current_url == 'about:blank'
+            assert USED_BROWSER == browser
+""" % (testit.base_url + '/simple.html')))
+    result = testit.runpytest()
+    assert result.parseoutcomes().get('passed') == 2
+
+
+def test_one_browser_by_test_if_specified(testit):
+    testit.makepyfile(py.code.Source(r"""
+        import pytest
+        USED_BROWSER = None
+        @pytest.mark.run(order=1)
+        def test_first(browser):
+            global USED_BROWSER
+            USED_BROWSER = browser
+        @pytest.mark.run(order=2)
+        def test_second(browser):
+            assert USED_BROWSER != browser
+"""))
+    result = testit.runpytest('--session-scoped-browser=false')
+    assert result.parseoutcomes().get('passed') == 2
